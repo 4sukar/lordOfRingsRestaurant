b7e3d3fec71162d41fb9d3a69cee79d4c1a7bc9a
Jhuan <4suakrsz@gmail.com>
2026-08-01 14:46:10 -0300

    feat: validating unique name and description

--- src/dishes/dish.service.ts
+++ src/dishes/dish.service.ts
@@ -1,13 +1,22 @@
 import { error } from "console";
 import { CreavteDishDTO, Dish } from "./dish";
-import { DishRepository } from "./dish.repository";
+import { DishRepository, PrismaDishRepository } from "./dish.repository";
 
 export class DishService{
     constructor(private dishRepo:DishRepository){
 
     }
     async create(payload: CreavteDishDTO):Promise<Dish>{
-      throw new Error("testando erros")
+        const dishes = await this.dishRepo.find()
+
+        if (dishes.some(dish => dish.name === payload.name)) {
+          throw new Error("A dish with this name already exists")
+        }
+
+        if (dishes.some(dish => dish.description === payload.description)) {
+          throw new Error("A dish with this description already exists")
+        }
+
         return this.dishRepo.create(payload)
     }
     async find():Promise<Dish[]>{
